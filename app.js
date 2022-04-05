c433011b4a6d09660dadecf735d63adc3797a7bc
Aishwarya <aishuu_02@outlook.com>
2022-04-05 09:13:14 +0530

    added login and register

---
+++ app.js
@@ -0,0 +1,63 @@
+const express= require('express')
+const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
+const path = require('path');
+const session = require('express-session');
+const userRoutes=require('./routes/users');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require("./models/users");
+const flash = require('connect-flash');
+const methodOverride = require('method-override')
+const homeRoutes = require('./routes/home');
+mongoose.connect('mongodb://localhost:27017/canteen', {
+   // useNewUrlParser: true,
+    useUnifiedTopology: true,
+   // useCreateIndex: true,
+   // useFindAndModify:false
+    });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('database connected')
+});
+
+const app = express();
+
+app.engine('ejs',ejsMate)
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'))
+
+ app.use(express.urlencoded({extended: true}));
+ app.use(methodOverride('_method'));
+ app.use(express.static(path.join(__dirname,'public')))
+
+
+app.use(session({ cookie: { maxAge: 60000 }, 
+  secret: 'woot',
+  resave: false, 
+  saveUninitialized: true}));
+
+  app.use(flash());
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+  passport.use(new LocalStrategy(User.authenticate()));
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
+  
+  
+  app.use((req,res,next)=>{
+    res.locals.currentUser = req.user;
+    res.locals.alert=req.flash('alert');
+    res.locals.success= req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+  })
+
+
+app.listen(3000,(req,res)=>{
+    console.log('connecting to port 3000!');
+})
+app.use('/',userRoutes);
+app.use('/home',homeRoutes);
---
+++ routes/users.js
@@ -0,0 +1,62 @@
+const express= require('express')
+const router = express.Router();
+const passport = require('passport');
+const User = require('../models/users');
+
+
+
+router.get('/register',(req,res)=>{
+    res.render('users/register');
+})
+
+router.post('/register',async(req,res)=>{
+    try{
+const {username,password,repassword}=req.body;
+console.log(username);
+req.session.currentuser = username;
+if (password !== repassword){
+    try{
+    throw 'passwords must be same';
+    }catch(e){
+    req.flash('error',e);
+    return res.redirect('register');
+    }
+} 
+const user = new User({username,role:"User"});
+const registeredUser = await User.register(user,password);
+req.login(registeredUser,err=>{
+    if(err)return next(err);
+    res.redirect('/home'); 
+})
+}
+catch(e){
+ req.flash('error','username not available')
+ res.redirect('register');
+    }
+})
+
+router.get('/login',(req,res)=>{
+    res.locals.currentUser = req.user;
+    res.render('users/login');
+})
+router.post('/login',passport.authenticate('local',{failureFlash: true,failureRedirect:'/login'}),(req,res)=>{
+ try{
+    const  username = req.body.username; 
+    req.session.currentuser=username;
+    const redirectUrl = req.session.returnTo || '/home'
+    delete req.session.returnTo;
+    if(redirectUrl === '/home'){
+    return res.redirect('/home')
+    }
+     return res.redirect(redirectUrl);
+ }catch(e){
+ req.flash('error','incorrect username or password') 
+ }
+  
+})
+ router.get('/logout',(req,res)=>{
+     req.logout();
+     res.redirect('/login');
+ })
+
+module.exports = router;
